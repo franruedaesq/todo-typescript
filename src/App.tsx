748dd31779f53034e37f226253dab3e8304638bb
franruedaesq <franruedaesq@gmail.com>
2021-10-26 23:55:55 -0300

    feat: add new task, delete task, check task

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import './App.css'
 
-function App() {
+
+type FormElement = React.FormEvent<HTMLFormElement>;
+
+interface ITask {
+  name: string;
+  done: boolean;
+  id: number;
+}
+
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState<string>('');
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  const taskInput = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: FormElement) => {
+    e.preventDefault();
+    addTask(newTask);
+    setNewTask('')
+    taskInput.current?.focus()
+  }
+
+  const addTask = (name: string) => {
+    const newTasks: ITask[] = [...tasks, { name, done: false, id: new Date().getTime() }];
+    setTasks(newTasks);
+  }
+
+  const toggleDoneTask = (task: ITask): void => {
+    const newTasks: ITask[] = [...tasks].map(t => {
+      if (t.id === task.id) {
+        t.done = !task.done
+      }
+      return t
+    })
+    setTasks(newTasks)
+  }
+
+  const removeTask = (task: ITask): void => {
+    const newTasks: ITask[] = [...tasks].filter(t => t.id !== task.id)
+    setTasks(newTasks)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="card">
+        <div className="card-body">
+          <form onSubmit={handleSubmit} >
+            <input type="text" onChange={e => setNewTask(e.target.value)} value={newTask} ref={taskInput} />
+            <button className="button-save">Save</button>
+          </form>
+        </div>
+      </div>
+      <ul>
+        {tasks.map(task =>
+          <li key={task.id}>
+            <h3 className={task.done ? 'done' : ''} >{task.name}</h3>
+            <div><button className="button-check" onClick={() => toggleDoneTask(task)} >{task.done ? '✔' : '✗'}</button>
+          <button className="removeTask" onClick={() => removeTask(task)} >🗑</button></div>
+          </li>)}
+      </ul>
+    </>
   );
 }
 
